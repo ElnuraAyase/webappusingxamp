13fc5dc717dbef3664c529024b2fd4d75922123c
Elnura <elya.ayase@gmail.com>
2024-02-19 01:04:09 +0400

    Create schedule.php

---
+++ schedule.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>English Classes Portal - Schedule</title>
+
+    <link rel="stylesheet" href="../CSS_EM/styleMirzayeva.css">
+    <link rel="icon" href="../IMG_EM/logo.png">
+</head>
+<body style="background-size: cover; background-image: url('../IMG_EM/k.jpg'); background-repeat: no-repeat; background-position: center;">
+
+    <header>
+        <div class="container">
+            <a href="index_Mirzayeva.html"><img src="../IMG_EM/logo.png" alt="English Classes Logo"></a>
+
+            <h1 class="animated" id="title">English Classes Portal - Schedule</h1>
+
+            <nav class="nav">
+                <div class="menu">
+                    <a href="index_Mirzayeva.html" class="btn-group">
+                        <span class="btn">Home</span>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section class="calendar">
+
